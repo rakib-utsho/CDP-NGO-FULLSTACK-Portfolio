ccbff1b8dcae32fdb6787079bd358a50e66bb247
rakib-utsho <rakibutsho1920@gmail.com>
2025-04-18 19:37:58 +0600

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUP from "./pages/SignUP";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 
 const App = () => {
   return (
-    <div className='text-2xl font-bold'>App</div>
-  )
-}
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUP />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
-export default App
+export default App;
